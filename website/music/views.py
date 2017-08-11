ae8c5ba843a54d0ffcf9f9d4c6cd7bbf4b036d0b
Mohit Sharma <mohit_creed@yahoo.com>
2017-08-11 17:31:07 +0530

    First commit

---
+++ website/music/views.py
@@ -0,0 +1,19 @@
+from django.views import generic
+from .models import Album, Song
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+
+class IndexView(generic.ListView):
+    template_name = 'music/index.html'
+    context_object_name = 'all_albums'
+
+    def get_queryset(self):
+        return Album.objects.all()
+
+class DetailView(generic.DetailView):
+    model = Album
+    template_name = 'music/detail.html'
+
+class AlbumCreate(CreateView):
+    model = Album
+    fields = ['artist', 'title', 'genre', 'logo']
